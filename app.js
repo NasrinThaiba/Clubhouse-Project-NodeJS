c3bb89736e47f7d916aa5293ec283143616e297d
Nasrin Thaiba <nasrinthaiba3@gmail.com>
2026-05-06 14:40:50 +0530

    updated

--- app.js
+++ app.js
@@ -5,48 +5,51 @@ const mongoose = require("mongoose");
 const passport = require("./passport");
 const session = require("express-session");
 const flash = require("connect-flash");
+const dns = require("dns").promises;
 
 const indexRouter = require("./routes/index");
-const authRouter = require("./routes/auth")
+const authRouter = require("./routes/auth");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT;
 const SECRET_KEY = process.env.SECRET_KEY;
 
-mongoose.connect(DB_URL)
-.then(()=> console.log("DB is connected Successfully!"))
-.catch((err) => console.log("DB Error", err))
+mongoose
+  .connect(DB_URL)
+  .then(() => console.log("DB is connected Successfully!"))
+  .catch((err) => console.log("DB Error", err));
 
 const app = express();
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static("public"))
-
-app.use(express.urlencoded({extended : false}))
-
-app.use(session({
-  secret : SECRET_KEY,
-  resave : false,
-  saveUninitialized : false
-}))
+app.use(
+  session({
+    secret: SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
-
 app.use(flash());
 
-// Make currentUser available in all views
+
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.error = req.flash("error");
   res.locals.success = req.flash("success");
   next();
-})
+});
 
 app.use("/", indexRouter);
 app.use("/", authRouter);
 
 app.listen(PORT, () => {
-  console.log(`Server is connected to the Port : ${PORT}`)
-})
+  console.log(`Server is connected to the Port : ${PORT}`);
+});
